996343400ce5c862798b6ada0328b7c1c4b9d5cb
arielmarie <arielstolz1@gmail.com>
2025-04-22 16:09:08 +0200

    Adding selenium common exceptions

--- Day50-tinder-swip-bot/main.py
+++ Day50-tinder-swip-bot/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 from dotenv import load_dotenv
 import os
 from time import sleep
