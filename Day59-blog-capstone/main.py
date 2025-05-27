3194cc8710d780ced643227d12f3ee168e728ac8
Zan Grjol <zan.grjol@zebedee.io>
2025-05-27 19:00:45 +0200

    Day 59 - Inserts blog post data from API json file, list of multiple blog posts active.

--- Day59-blog-capstone/main.py
+++ Day59-blog-capstone/main.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+blog_url = "https://api.npoint.io/b329384f235cf8d55795"
+response = requests.get(blog_url)
+all_posts = response.json()
 
 
 # Creating variable paths and converting user inputs
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/about")
@@ -15,9 +20,9 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/post")
-def post():
-    return render_template("post.html")
+@app.route("/<int:post_id>")
+def post(post_id):
+    return render_template("post.html", posts=all_posts, post_id=post_id)
 
 
 @app.route("/contact")
