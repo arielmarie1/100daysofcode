e06b9a004432ad5bac365d5a9945c1dc1f879b9b
arielmarie <arielstolz1@gmail.com>
2025-06-13 16:34:56 +0200

    Sends email when someone fills out contact form on blog.

--- Day59-blog-capstone/main.py
+++ Day59-blog-capstone/main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
 import requests
 
 app = Flask(__name__)
@@ -7,6 +11,10 @@ blog_url = "https://api.npoint.io/b329384f235cf8d55795"
 response = requests.get(blog_url)
 all_posts = response.json()
 
+# Loading email
+load_dotenv()
+my_email = os.getenv("MY_EMAIL")
+password = os.getenv("OTP")
 
 # Creating variable paths and converting user inputs
 @app.route("/")
@@ -32,7 +40,20 @@ def receive_data():
         email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
-        print(f"Name: {name}\nEmail: {email}\nPhone number: {phone}\nMessage: {message}")
+
+        # Email message
+        msg = EmailMessage()
+        msg['From'] = email
+        msg['To'] = my_email
+        msg["Subject"] = "Contact Form - Ariel's Blog"
+        msg.set_content(f"Name: {name}\nEmail: {email}\nPhone number: {phone}\nMessage: {message}")
+
+        # Sending email
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.send_message(msg)
+
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
