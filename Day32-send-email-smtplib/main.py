35e22b02db3a1b7f4717ff60708636612996e0aa
arielmarie <arielstolz1@gmail.com>
2025-06-13 16:02:29 +0200

    Cleans up code using with

--- Day32-send-email-smtplib/main.py
+++ Day32-send-email-smtplib/main.py
@@ -7,8 +7,11 @@ load_dotenv()
 my_email = os.getenv("MY_EMAIL")
 password = os.getenv("OTP")
 
-connection = smtplib.SMTP("smtp.gmail.com", port=587)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=my_email, msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email,
+        msg="Subject:Hello\n\n Hello World!"
+    )
