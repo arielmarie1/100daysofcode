d23e20d97e220a4b8a83cbd1221c7d4f4c7beb5b
arielmarie <arielstolz1@gmail.com>
2025-06-13 18:09:49 +0200

    Checks if login matches admin login info and reroutes to success or denied page accordingly.

--- Day61-advanced-wtforms/main.py
+++ Day61-advanced-wtforms/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[Email(), Length(min=4), DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
@@ -22,7 +22,11 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
 
 
