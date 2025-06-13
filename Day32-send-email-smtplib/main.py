bbe7bc5d62ee5e91ba4acc6d82d944209ae1be1d
arielmarie <arielstolz1@gmail.com>
2025-06-13 16:28:42 +0200

    Sends Monday Motivation email.

--- Day32-send-email-smtplib/main.py
+++ Day32-send-email-smtplib/main.py
@@ -1,17 +1,33 @@
 import smtplib
+from email.message import EmailMessage
+import datetime as dt
+import random
 from dotenv import load_dotenv
 import os
 
-# Adding link to google form for data entry
+# Loading email
 load_dotenv()
 my_email = os.getenv("MY_EMAIL")
 password = os.getenv("OTP")
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=my_email,
-        msg="Subject:Hello\n\n Hello World!"
-    )
+# Monday Motivational quote
+now = dt.datetime.now()
+if now.weekday() == 0:
+
+    # Open file quotes.txt
+    with open("quotes.txt", encoding="utf-8") as quotes:
+        all_quotes = quotes.readlines()
+        quote = random.choice(all_quotes)  # Randomly select a quote from file
+
+    # Email message
+    msg = EmailMessage()
+    msg['From'] = my_email
+    msg['To'] = my_email
+    msg["Subject"] = "Monday Motivation"
+    msg.set_content(quote)
+
+    # Sending email
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.send_message(msg)
