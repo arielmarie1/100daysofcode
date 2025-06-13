a21069e4190c1e2a5d84dcee6de08502252d1450
arielmarie <arielstolz1@gmail.com>
2025-06-13 17:16:11 +0200

    Creates login form using Flask WTForms

--- Day61-advanced-wtforms/main.py
+++ Day61-advanced-wtforms/main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
 
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
 
 @app.route("/")
@@ -11,7 +19,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
