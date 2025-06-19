acdc3007114782e1fa76f55572183967b05efb42
arielmarie <arielstolz1@gmail.com>
2025-06-19 13:22:02 +0200

    Creates form to add new cafe to list of cafes. Updates base to use bootstrap and css styling files. Adds routing functionality to buttons. Adds table to Cafes page.

--- Day62-forms-bootstrap-csv/main.py
+++ Day62-forms-bootstrap-csv/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField, DateTimeField, URLField
+from wtforms.validators import DataRequired, NumberRange, URL
 import csv
 
 
@@ -10,21 +10,42 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+cafeformheaders = ["cafe", "location", "open_time", "close_time",
+                   "coffee_rating", "wifi_rating", "power_rating"]
+
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
+    cafe = StringField(
+        'Cafe name',
+        validators=[DataRequired()])
+    location = URLField(
+        'Location URL',
+        validators=[DataRequired(), URL()])
+    open_time = DateTimeField(
+        'Opening time',
+        format="%I %p",
+        render_kw={"placeholder": "ex. 9 AM"},
+        validators=[DataRequired()])
+    close_time = DateTimeField(
+        'Closing time',
+        format="%I %p",
+        render_kw={"placeholder": "ex. 10 PM"},
+        validators=[DataRequired()])
+    coffee_rating = IntegerField(
+        'Coffee Rating 0-5',
+        validators=[DataRequired(),
+                    NumberRange(min=0, max=5)])
+    wifi_rating = IntegerField(
+        'Wifi Rating 0-5',
+        validators=[DataRequired(),
+                    NumberRange(min=0, max=5)])
+    power_rating = IntegerField(
+        'Power Outlet Rating 0-5',
+        validators=[DataRequired(),
+                    NumberRange(min=0, max=5)])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
 
-
-# all Flask routes below
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -38,17 +59,18 @@ def add_cafe():
     # Exercise:
     # Make the form write a new row into cafe-data.csv
     # with   if form.validate_on_submit()
-    return render_template('add.html', form=form)
+    return render_template('add.html', form=form, headers=cafeformheaders)
 
 
 @app.route('/cafes')
 def cafes():
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
+      #  headers = next(csv_data) # first row
         list_of_rows = []
         for row in csv_data:
             list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    return render_template('cafes.html', cafes=list_of_rows, headers=cafeformheaders)
 
 
 if __name__ == '__main__':
