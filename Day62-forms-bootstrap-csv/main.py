e04798c096b8f392ec85c001c785c61f166875e6
arielmarie <arielstolz1@gmail.com>
2025-06-19 15:10:52 +0200

    Fixes newline bug in csv file.

--- Day62-forms-bootstrap-csv/main.py
+++ Day62-forms-bootstrap-csv/main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, DateTimeField, URLField
-from wtforms.validators import DataRequired, NumberRange, URL
+from wtforms.validators import DataRequired, NumberRange, URL, InputRequired
+from datetime import datetime
 import csv
 
 
@@ -12,6 +13,16 @@ Bootstrap5(app)
 
 cafeformheaders = ["cafe", "location", "open_time", "close_time",
                    "coffee_rating", "wifi_rating", "power_rating"]
+
+
+def convert_rating(value, emoji):
+    try:
+        value = int(value)
+        if value == 0:
+            return "✘"
+        return emoji * value
+    except (ValueError, TypeError):
+        return "✘"
 
 
 class CafeForm(FlaskForm):
@@ -31,15 +42,15 @@ class CafeForm(FlaskForm):
         validators=[DataRequired()])
     coffee_rating = IntegerField(
         'Coffee Rating 0-5',
-        validators=[DataRequired(),
+        validators=[InputRequired(),
                     NumberRange(min=0, max=5)])
     wifi_rating = IntegerField(
         'Wifi Rating 0-5',
-        validators=[DataRequired(),
+        validators=[InputRequired(),
                     NumberRange(min=0, max=5)])
     power_rating = IntegerField(
         'Power Outlet Rating 0-5',
-        validators=[DataRequired(),
+        validators=[InputRequired(),
                     NumberRange(min=0, max=5)])
     submit = SubmitField('Submit')
 
@@ -53,14 +64,27 @@ def home():
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("Submitted")
         new_cafe = []
         for header in cafeformheaders:
             cafe = form[header].data
+            if header == "coffee_rating":
+                cafe = convert_rating(cafe, "☕")
+            if header == "wifi_rating":
+                cafe = convert_rating(cafe, "💪")
+            if header == "power_rating":
+                cafe = convert_rating(cafe, "🔌")
             new_cafe.append(cafe)
+        # Add new line if necessary
+        with open("cafe-data.csv", "rb+") as f:
+            f.seek(-1, 2)  # Go to the last byte of the file
+            last_char = f.read(1)
+            if last_char != b'\n':
+                f.write(b'\n')  # Only add a newline if it's missing
+
         with open("cafe-data.csv", mode="a", newline='', encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(new_cafe)
+
     return render_template('add.html', form=form, headers=cafeformheaders)
 
 
