63e2f8ae47c3358b0cc6b86f2533e037bd3ed8e1
arielmarie <arielstolz1@gmail.com>
2025-06-23 15:47:37 +0200

    Fixing requirements and virtual environments

--- Day63-SQL-databases/main.py
+++ Day63-SQL-databases/main.py
@@ -1,12 +1,45 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, NumberRange, InputRequired
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very_secret_key'
-Bootstrap5(app)
+bootstrap = Bootstrap(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-collection.db'
+
+# Create and Initialize extension
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+with app.app_context():
+    db.create_all()
+
+
+# Create website form
 all_books = []
 formheaders = ["title", "author", "rating"]
 
@@ -34,7 +67,13 @@ def add():
         for header in formheaders:
             book_dict[form[header].name] = form[header].data
         all_books.append(book_dict)
-        print(all_books)
+        print(book_dict.title)
+        # # Create a new record
+        # with app.app_context():
+        #     book1 = Book(id=1, title='Harry Potter', author='J.K. Rowling', rating=9.3)
+        #     # new_book = Book(title='Exhalation', author='Ted Chang', rating=9.4)
+        #     db.session.add(book1)
+        #     db.session.commit()
         return render_template("add.html", form=form, headers=formheaders, book_added=True)
     return render_template("add.html", form=form, headers=formheaders, book_added=False)
 
