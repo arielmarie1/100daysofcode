1799ed17a3b21f8a037fecc8ecf34ef611e80e13
arielmarie <arielstolz1@gmail.com>
2025-07-25 18:04:44 +0200

    Working on creating API get and post methods

--- Day66-build-your-own-API/main.py
+++ Day66-build-your-own-API/main.py
@@ -2,25 +2,16 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+import random as r
 
 app = Flask(__name__)
+
 
 # CREATE DB
 class Base(DeclarativeBase):
     pass
+
+
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(model_class=Base)
@@ -46,18 +37,61 @@ with app.app_context():
     db.create_all()
 
 
+def to_dict(self):
+    return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-# HTTP GET - Read Record
+@app.route("/random")
+def get_random_cafe():
+    with app.app_context():
+        cafes = db.session.execute(db.select(Cafe)).scalars().all()
+        random_cafe = r.choice(cafes)
+        return jsonify(cafe=to_dict(random_cafe))
 
-# HTTP POST - Create Record
 
-# HTTP PUT/PATCH - Update Record
+@app.route("/all")
+def get_all_cafes():
+    with app.app_context():
+        cafes = db.session.execute(db.select(Cafe).order_by(Cafe.name)).scalars().all()
+        all_cafes = [to_dict(cafe) for cafe in cafes]
+        return jsonify(cafes=all_cafes)
 
-# HTTP DELETE - Delete Record
+
+@app.route("/search")
+def find_cafe():
+    loc = request.args.get("loc")
+    with app.app_context():
+        cafes = db.session.execute(db.select(Cafe).where(Cafe.location == loc)).scalars().all()
+        all_cafes = [to_dict(cafe) for cafe in cafes]
+    if all_cafes:
+        return jsonify(cafes=all_cafes)
+    else:
+        statement = "Sorry, we don't have a cafe at that location"
+        return jsonify(error=statement), 404
+
+
+@app.route("/add", methods=["POST"])
+def add_cafe():
+    cafe = Cafe()
+    cafe.name = request.form.get("name")
+    cafe.map_url = request.form.get("map_url")
+    cafe.img_url = request.form.get("img_url")
+    cafe.location = request.form.get("location")
+    cafe.seats = request.form.get("seats")
+    cafe.has_toilet = request.form.get("has_toilet")
+    cafe.has_wifi = request.form.get("has_wifi")
+    cafe.has_sockets = request.form.get("has_sockets")
+    cafe.can_take_calls = request.form.get("can_take_calls")
+    cafe.coffee_price = request.form.get("coffee_price")
+
+    db.session.add(cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added cafe"})
 
 
 if __name__ == '__main__':
